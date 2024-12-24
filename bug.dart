d999eeb67773a86f4836e1d2ff756bd1a959780e
BugHunter-X <askzenai@gmail.com>
2024-12-24 19:30:06 +0100

    Add bug.dart file

---
+++ bug.dart
@@ -0,0 +1,15 @@
+```dart
+List<int> numbers = [1, 2, 3, 4, 5];
+int sum = numbers.reduce((a, b) => a + b);
+print(sum);
+
+//This code will work fine for positive integers.
+
+List<int> numbers2 = [-1,-2,-3,-4,-5];
+int sum2 = numbers2.reduce((a, b) => a + b);
+print(sum2); //Correctly prints -15
+
+List<int> numbers3 = [];
+int sum3 = numbers3.reduce((a, b) => a + b); // Throws an error
+print(sum3);
+```
